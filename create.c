2fbedab066118ce4419aef1af2159916d127b13a
prty1025 <prty1025@hotmail.com>
2018-07-04 18:01:11 +0900

    create

---
+++ create.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUFSIZE 100
+
+int main(int argc, char *argv[])
+{
+	int fd;
+        int count;
+        char wbuf[BUFSIZE];
+        char rbuf[BUFSIZE];
+        printf("argv[0]=%s\n",argv[0]);
+        printf("argv[1]=%s\n",argv[1]);
+        fd = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, \
+             S_IRWXU | S_IWGRP | S_IRGRP | S_IROTH);
+
+        if(fd==-1)
+        {
+        	 printf("file error!!\n");
+      	         return -1;
+        }
+
+       sprintf(wbuf,"Do not count the before they hatch.");
+       printf("%s\n",wbuf);
+       count = write(fd, wbuf, strlen(wbuf));
+        if(count<1)
+	{
+		 printf("file write error!!\n");
+	 	 return -1;
+	}
+	  //
+	 lseek(fd, 0, SEEK_SET);
+	 memset(rbuf,0,BUFSIZE);
+	 count = read(fd,rbuf,BUFSIZE);
+	 if(count<1)
+	 {
+	 	 printf("file read error!!\n");
+	   	 return -1;
+	 }
+	   close(fd);
+}
